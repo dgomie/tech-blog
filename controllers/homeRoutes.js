dfd94284f536c1bf050e90364393ecf511415c9a
Danny <danny.f.gomez@gmail.com>
2024-05-12 21:58:53 -0400

    Add cookies and title to renders in homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,20 +2,28 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+  res.render('homepage', {
+    title: 'Tech Blog',
+    loggedIn: req.session.loggedIn,
+  });
 });
 
 router.get("/login", (req, res) => {
-  // if (req.session.loggedIn) {
-  //   res.redirect("/");
-  //   return;
-  // }
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-  res.render("login");
+  res.render("login", {
+    title: "Login"
+  });
 });
 
 router.get("/dashboard", (req, res) => {
-  res.render('dashboard')
+  res.render('dashboard', {
+    title: 'Dashboard',
+    loggedIn: req.session.loggedIn,
+  })
 })
 
 
