ae82e0d4911181a608b9707999876d6d4b0ba106
Danny <danny.f.gomez@gmail.com>
2024-05-13 14:59:36 -0400

    Linked db post data to blog-cards

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const { User, Post } = require('../models');
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
+  try {
+    const dbPostData = await Post.findAll();
+    const posts = dbPostData.map((post) =>
+      post.get({ plain: true })
+    );
   res.render('homepage', {
     title: 'Tech Blog',
     loggedIn: req.session.loggedIn,
+    posts
   });
+} catch (err) {
+  console.log(err);
+  res.status(500).json(err);
+}
+
 });
 
 router.get("/login", (req, res) => {
@@ -13,7 +25,6 @@ router.get("/login", (req, res) => {
     res.redirect("/");
     return;
   }
-
   res.render("login", {
     title: "Login"
   });
