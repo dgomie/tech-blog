1a51bb6041c59a5ecd6cfb16ab968579c9e1a921
Danny <danny.f.gomez@gmail.com>
2024-05-13 16:10:31 -0400

    created post route for new blog posts

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { User, Post } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.status(200).send(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.status(200).send(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+    res.json(newPost);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+module.exports = router;
