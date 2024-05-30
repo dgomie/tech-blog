bdaaa786d5e1e5d358a64fdec0ac82ee80f9d498
Danny <danny.f.gomez@gmail.com>
2024-05-29 21:12:15 -0400

    add userid to local storage on signup

--- public/js/login.js
+++ public/js/login.js
@@ -25,39 +25,8 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-  console.log("sign up")
-
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  console.log(username, email, password)
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ username, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      const data = await response.json()
-      console.log(data)
-      localStorage.setItem("userId", data.user.id)
-      document.location.replace('/dashboard');
-    } else {
-      console.log(response)
-      alert('Failed to sign up.');
-    }
-  }
-};
-
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,28 +1,32 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
-  console.log("sign up");
+  console.log("sign up")
 
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
-  console.log(username, email, password);
+  console.log(username, email, password)
 
   if (username && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
+    const response = await fetch('/api/users', {
+      method: 'POST',
       body: JSON.stringify({ username, email, password }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace("/dashboard");
+      const data = await response.json()
+      console.log(data)
+      localStorage.setItem("userId", data.id)
+      document.location.replace('/dashboard');
     } else {
-      console.log(response);
-      alert("Failed to sign up.");
+      console.log(response)
+      alert('Failed to sign up.');
     }
   }
 };
+
 
 document
   .querySelector(".signup-form")
