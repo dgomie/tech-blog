b50e5495babe8601f2558a53caed0c530953afd0
Danny <danny.f.gomez@gmail.com>
2024-05-31 22:06:20 -0400

    add ability to go to individual post by clicking on post on homepage

--- models/index.js
+++ models/index.js
@@ -13,12 +13,12 @@ Post.belongsTo(User, {
 })
 
 Post.hasMany(Comment, {
-    foreignKey: 'comment_id',
+    foreignKey: 'post_id',
     onDelete: "CASCADE"
 })
 
 Comment.belongsTo(Post, {
-    foreignKey: 'comment_id',
+    foreignKey: 'post_id', 
     onDelete: "CASCADE"
 })
 
